d6d0f77d9b8469c06f94b8a78bc30dfe04bbad62
derekdhuynh <derek.5922.dh@gmail.com>
2023-10-04 18:30:07 -0400

    frontend injected into index.html

---
+++ src/main/js/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Navbar from './NavBar';
+import Box from './Box';
+// import '../resources/static/App.css';
+function App() {
+    return (
+        <div className="App">
+            <Navbar />
+            <div className="content">
+                <Box />
+            </div>
+        </div>
+    );
+}
+
+ export default App;
+
+
+//import React, { useState, useEffect } from 'react';
+//import axios from 'axios';
+//
+//function App() {
+//    const [data, setData] = useState('');
+//
+//    useEffect(() => {
+//        axios.get('/employees')
+//            .then(response => {
+//                setData(response.data);
+//            })
+//            .catch(error => {
+//                console.error('Error fetching data:', error);
+//            });
+//    }, []);
+//
+//    return (
+//        <div>
+//            <p>Data from Spring Boot:</p>
+//            <p>{data}</p>
+//        </div>
+//    );
+//}
+//
+//export default App;
---
+++ src/main/js/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import logoImage from './Images/Logo.png';
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li className="left-content"> </li>
+                <li><a href="#" className="nav-link-home">Home</a></li>
+                <li><a href="#" className="nav-link-exit">Exit</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
