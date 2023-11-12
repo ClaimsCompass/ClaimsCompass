09a415b6a95a6999ad661f931ef184e79779c9fa
Krisha-11 <krisha.kalsi@gmail.com>
2023-11-12 16:33:23 -0500

    Created LoginRepository with findBy and existsBy methods

---
+++ src/main/java/Login/LoginRepository.java
@@ -0,0 +1,12 @@
+package Login;
+
+import entities.ClaimsExaminer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface LoginRepository extends JpaRepository<ClaimsExaminer, Long> {
+    Optional<ClaimsExaminer> findByFirstName(String firstName);
+    Optional<ClaimsExaminer> findByUsername(String username);
+    Boolean existsByFirstName(String firstName);
+    Boolean existsByUsername(String username);
+}
