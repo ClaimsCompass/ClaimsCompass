9a249c5065a74f9792463f39e4fa224aecd69243
Krisha-11 <krisha.kalsi@gmail.com>
2023-11-12 19:59:03 -0500

    removed unused import GetMapping and Autowired statements

--- src/main/java/Login/LoginController.java
+++ src/main/java/Login/LoginController.java
@@ -1,7 +1,5 @@
 package Login;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
