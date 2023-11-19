0e2d91af7cb18af4011d7ec3253e900aad1c8674
Pratibha <108570654+praticodes@users.noreply.github.com>
2023-11-19 20:25:02 +0100

    Create interface for ClaimState

--- src/main/java/com/securian/creditcompass/ClaimState.java
+++ src/main/java/com/securian/creditcompass/ClaimState.java
@@ -1,5 +1,7 @@
 package com.securian.creditcompass;
 
 public interface ClaimState {
+    void assignToClaimsExaminer(ClaimsExaminer examiner, Claim claim);
+    void calculateScore(Claim claim);
     void processClaim(Claim claim);
 }
