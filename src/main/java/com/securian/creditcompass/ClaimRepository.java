5c48880126393c4e115f0a8fb95f4477da8946cd
Andrew Xie <anrui.xie@gmail.com>
2023-11-22 19:38:07 -0500

    Deleted the old files that weren't in use

--- src/main/java/com/securian/creditcompass/ClaimRepository.java
+++
@@ -1,6 +0,0 @@
-package com.securian.creditcompass;
-
-import com.securian.creditcompass.CrudRepository;
-import com.securian.creditcompass.Claim;
-
-public interface ClaimRepository extends CrudRepository<Claim, Integer> {}
