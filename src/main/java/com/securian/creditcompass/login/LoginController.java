70eedef0e6b82e84fec1e8aa8c233210626b1245
MaryamTaj <tajm48822@gmail.com>
2023-11-22 21:30:16 -0500

    Implementing CA (and fixing 400 bad request error) in login use case

--- src/main/java/com/securian/creditcompass/login/LoginController.java
+++ src/main/java/com/securian/creditcompass/login/LoginController.java
@@ -3,6 +3,8 @@ package com.securian.creditcompass.login;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 public class LoginController {
     private final LoginInteractor loginService;
@@ -13,8 +15,10 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Object> login(@RequestParam ("username") String username, @RequestParam ("password") String password) {
-       LoginInputData loginInputData = new LoginInputData(username, password);
+    public ResponseEntity<Object> login(@RequestBody Map<String, String> userInput) {
+        String username = userInput.get("username");
+        String password = userInput.get("password");
+        LoginInputData loginInputData = new LoginInputData(username, password);
         return loginService.authenticate(loginInputData);
     }
 
