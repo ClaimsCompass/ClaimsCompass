5adf5250d605a9825e089612028f129f0f1eff1f
Krisha-11 <krisha.kalsi@gmail.com>
2023-11-25 12:34:09 -0500

    initialized test file for claim entity

---
+++ src/test/java/com/securian/creditcompass/entities/ClaimTest.java
@@ -0,0 +1,65 @@
+package com.securian.creditcompass.entities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClaimTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getClaimType() {
+    }
+
+    @Test
+    void getClaimDetails() {
+    }
+
+    @Test
+    void getClaimAmt() {
+    }
+
+    @Test
+    void getComplexityScore() {
+    }
+
+    @Test
+    void getUrgencyScore() {
+    }
+
+    @Test
+    void setComplexityScore() {
+    }
+
+    @Test
+    void setUrgencyScore() {
+    }
+
+    @Test
+    void getCreationDateTime() {
+    }
+
+    @Test
+    void setTotalScore() {
+    }
+
+    @Test
+    void getTotalScore() {
+    }
+
+    @Test
+    void isProcessed() {
+    }
+
+    @Test
+    void processClaim() {
+    }
+}
