86aa9733d2b9c2e0d906a04d7c4c47dcc098ab03
Krisha-11 <krisha.kalsi@gmail.com>
2023-11-26 01:09:17 -0500

    added input data constructor

--- src/main/java/com/securian/creditcompass/login/LoginInputData.java
+++ src/main/java/com/securian/creditcompass/login/LoginInputData.java
@@ -1,4 +1,14 @@
 package com.securian.creditcompass.login;
 
-public class LoginInputData {
+import lombok.Getter;
+
+@Getter
+public class LoginInputData{
+    private final String username;
+    private final String password;
+
+    public LoginInputData(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
