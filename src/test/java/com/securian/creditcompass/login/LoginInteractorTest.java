77ec449e7153c4f631202deada2079508a92d65b
Krisha-11 <krisha.kalsi@gmail.com>
2023-11-26 10:04:06 -0500

    extended LoginInteractorTest with Mockito Extension

---
+++ src/test/java/com/securian/creditcompass/login/LoginInteractorTest.java
@@ -0,0 +1,20 @@
+package com.securian.creditcompass.login;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class LoginInteractorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void authenticate() {
+    }
+}
