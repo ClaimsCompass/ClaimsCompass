ebe3144bab6ff7186a71390b30af28c5bb81a286
Krisha-11 <krisha.kalsi@gmail.com>
2023-11-26 10:52:23 -0500

    Added testAuthenticateSuccess, testWrongPasswordThrowsException, testExaminerNotFoundThrowsException

--- src/test/java/com/securian/creditcompass/login/LoginInteractorTest.java
+++ src/test/java/com/securian/creditcompass/login/LoginInteractorTest.java
@@ -1,20 +1,81 @@
 package com.securian.creditcompass.login;
 
+import com.securian.creditcompass.entities.ClaimsExaminer;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.naming.AuthenticationException;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class LoginInteractorTest {
+    @Mock
+    private LoginDataAccessInterface loginDataAccessInterface;
+    @InjectMocks
+    // injects all mocks into LoginInteractor object
+    private LoginInteractor loginInteractor;
 
     @BeforeEach
     void setUp() {
+        // instantiate new LoginInteractor object before each test
+        loginInteractor = new LoginInteractor(loginDataAccessInterface);
     }
 
     @Test
-    void authenticate() {
+    void testAuthenticateSuccess() throws AuthenticationException {
+        //GIVEN
+        // setting up the instance input param
+        LoginInputData loginInputData = new LoginInputData("janeDoe", "passcode");
+        // creating a stub for call to database
+        Optional<ClaimsExaminer> stubExaminer = Optional.of(new ClaimsExaminer("janeDoe", "passcode", "jane", 123L));
+        // when the findByUsername method is called, return the stub
+        when(loginDataAccessInterface.findByUsername("janeDoe")).thenReturn(stubExaminer);
+
+        //WHEN METHOD CALLED
+        boolean responseSuccess = loginInteractor.authenticate(loginInputData);
+        //ASSERT
+        assertThat(responseSuccess).isTrue();
+    }
+
+    @Test
+    void testWrongPasswordThrowsException() throws AuthenticationException {
+        //GIVEN
+        // setting up the instance input param
+        LoginInputData loginInputData = new LoginInputData("janeDoe", "passcode");
+        // creating a stub for call to database
+        Optional<ClaimsExaminer> stubExaminer = Optional.of(new ClaimsExaminer("janeDoe", "wrongPassword", "jane", 123L));
+        // when the findByUsername method is called, return the stub
+        when(loginDataAccessInterface.findByUsername("janeDoe")).thenReturn(stubExaminer);
+
+        //ASSERT
+        // here using Assertions.assertThrows to test that method throws an AuthenticationException only
+        AuthenticationException thrownInvalid = assertThrows(AuthenticationException.class, () -> loginInteractor.authenticate(loginInputData));
+        assertEquals("Invalid credentials.", thrownInvalid.getMessage());
+    }
+
+    @Test
+    void testExaminerNotFoundThrowsException() throws AuthenticationException{
+        //GIVEN
+        // setting up the instance input param
+        LoginInputData loginInputData = new LoginInputData("janeDoe", "passcode");
+
+        // Call to database should return null value, optionalExaminer.ispresent() will be false
+        when(loginDataAccessInterface.findByUsername("janeDoe")).thenReturn(Optional.empty());
+
+        // ASSERT
+        AuthenticationException thrownIncorrect = assertThrows(AuthenticationException.class, () -> loginInteractor.authenticate(loginInputData));
+        assertEquals("Incorrect credentials.", thrownIncorrect.getMessage());
     }
 }
