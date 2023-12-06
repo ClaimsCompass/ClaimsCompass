cc75b87b4588d4990850f9a32c33b00a208b9ee3
Krisha-11 <krisha.kalsi@gmail.com>
2023-12-06 12:36:54 -0500

    prompted by IntelliJ. Assuming our repository will always be populated with Examiners, adding NotNull annotation is appropriate

--- src/main/java/com/securian/creditcompass/dataAccess/ExaminerRepository.java
+++ src/main/java/com/securian/creditcompass/dataAccess/ExaminerRepository.java
@@ -1,9 +1,11 @@
 package com.securian.creditcompass.dataAccess;
 
 import com.securian.creditcompass.entities.ClaimsExaminer;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +14,7 @@ public interface ExaminerRepository extends JpaRepository<ClaimsExaminer, Long> 
     Optional<ClaimsExaminer> findByUsername(String username);
     Boolean existsByFirstName(String firstName);
     Boolean existsByUsername(String username);
+
+    @NotNull
+    List<ClaimsExaminer> findAll();
 }
