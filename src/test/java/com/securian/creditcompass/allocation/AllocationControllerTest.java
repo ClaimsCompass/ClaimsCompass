339fed3e1415ad5f520e0a6f56cc91c81db8efd8
Krisha-11 <krisha.kalsi@gmail.com>
2023-12-06 12:35:40 -0500

    checked for boolean return of controller as a check for AllocationService being invoked

--- src/test/java/com/securian/creditcompass/allocation/AllocationControllerTest.java
+++ src/test/java/com/securian/creditcompass/allocation/AllocationControllerTest.java
@@ -5,11 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -22,7 +19,10 @@ class AllocationControllerTest {
     @Test
     void execute() throws Exception {
         // perform the post request to /assign endpoint and check that the status code is 200
-        mvc.perform(post("/assign")).andExpect(status().isOk());
+        // also check that the response body is "true" to signify if the controller method executes successfully
+        mvc.perform(post("/assign")).andExpect(status().isOk())
+                .andExpect(content().string("true"));
+
 
     }
 }
