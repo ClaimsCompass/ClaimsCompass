63602b52233a6414df5915a56f793fd134c90ecc
MaryamTaj <tajm48822@gmail.com>
2023-12-06 16:04:10 -0500

    Adding a green navbar to the dashboard

--- frontend/src/dashboardModernize/Dashboard.js
+++ frontend/src/dashboardModernize/Dashboard.js
@@ -8,12 +8,14 @@ import ClaimsBreakdown from './components/ClaimsBreakdown';
 import RecentlyProcessed from './components/RecentlyProcessed';
 import ClaimsTable from './components/ClaimsTable';
 import YearlyClaimsStats from './components/YearlyClaimsStats';
+import Navbar from "./components/Navbar";
 
 
 const Dashboard = () => {
   return (
     <DashboardContainer title="Your Claims" description="this is Dashboard">
-      <Box>
+      <Navbar></Navbar>
+      <Box sx={{marginTop: '100px'}}>
         <Grid container spacing={3}>
           <Grid item xs={12} lg={8}>
             <ClaimsTable />
