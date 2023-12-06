9aaf27cf87cb35e02afc1f4951fdfb9f45fa1297
Pratibha <108570654+praticodes@users.noreply.github.com>
2023-12-07 00:23:26 +0100

    dashboard javadocs

--- src/main/java/com/securian/creditcompass/useCases/dashboard/DashboardInputData.java
+++ src/main/java/com/securian/creditcompass/useCases/dashboard/DashboardInputData.java
@@ -11,6 +11,10 @@ public class DashboardInputData {
     private Boolean displayProcessed;
 
     public DashboardInputData(String username, Boolean displayProcessed) {
+        /*
+        @param username: the username of the user to get the dashboard data for
+        @param displayProcessed: whether to get the processed or unprocessed claims
+         */
         this.username = username;
         this.displayProcessed = displayProcessed;
     }
