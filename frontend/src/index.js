5c7bbb3b52ed2a9b90f1558e14e8d53452f79452
derekdhuynh <derek.5922.dh@gmail.com>
2023-12-07 01:14:15 -0500

    hopefully fixed hash router?

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
  */
 root.render(
   <React.StrictMode>
-      <HashRouter basename={"/ClaimsCompass"}>
+      <HashRouter>
           <App />
       </HashRouter>
   </React.StrictMode>
